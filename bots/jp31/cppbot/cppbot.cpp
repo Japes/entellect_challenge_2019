faab7d69cf70b31e4f22cb4c2aa16e1ae718845c
JP da Conceicao <jpdacon@gmail.com>
2019-09-01 14:02:53 +0200

    Template for next bot

---
+++ bots/jp31/cppbot/cppbot.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Utilities.hpp"
+#include "Bot.hpp"
+#include "Evaluators/ScoreEvaluator.hpp"
+
+std::string executeRound(Bot& bot, std::string& roundNumber)
+{
+    const std::string filePath = "./rounds/" + roundNumber + "/state.json";
+    try {
+        
+        rapidjson::Document roundJSON = Utilities::ReadJsonFile(filePath);
+        return "C;" + roundNumber + ";" + bot.runStrategy(roundJSON) + "\n";
+
+    } catch(...) {
+        return "C;" + roundNumber + ";error executeRound \n";
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int playThroughDepth{12};
+    int nodeDepth{1};
+    int dirtsForBanana{100}; //effectively, turn this off
+    int clearSpaceForHeuristic{-1}; //if everything is clear for this distance, use heuristic
+    uint64_t mcTime_ns{880000000};
+    float mc_c{std::sqrt(2)};
+    int mc_runsBeforeClockCheck{50};
+
+    ScoreEvaluator eval;
+    Bot bot(&eval, playThroughDepth, nodeDepth, dirtsForBanana, clearSpaceForHeuristic, mcTime_ns, mc_c, mc_runsBeforeClockCheck);
+
+    for (std::string roundNumber; std::getline(std::cin, roundNumber);) 
+    {
+        std::cout << executeRound(bot, roundNumber);
+    }
+}
--- src/GameEngine/Commands/Command.cpp
+++ src/GameEngine/Commands/Command.cpp
@@ -1,6 +1,6 @@
 #include "Command.hpp"
 
-const std::string Command::latestBot = "JP30";
+const std::string Command::latestBot = "JP31";
 
 //to help with debugging...
 std::ostream & operator << (std::ostream &out, std::shared_ptr<Command> &cmd)
