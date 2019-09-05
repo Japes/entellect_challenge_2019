9beb54271cd56384a881f4a78cf3b9784bd56dfb
JP da Conceicao <jpdacon@gmail.com>
2019-09-05 08:46:36 +0200

    woops forgot

--- src/GameEngine/Commands/Command.cpp
+++ src/GameEngine/Commands/Command.cpp
@@ -1,6 +1,6 @@
 #include "Command.hpp"
 
-const std::string Command::latestBot = "JP34";
+const std::string Command::latestBot = "JP35";
 
 //to help with debugging...
 std::ostream & operator << (std::ostream &out, std::shared_ptr<Command> &cmd)
