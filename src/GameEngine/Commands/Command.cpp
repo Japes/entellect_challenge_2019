b7e4101714140053226ca9d5a361e816c390ea0b
JP da Conceicao <jpdacon@gmail.com>
2019-09-07 15:55:27 +0200

    Add bot for this

--- src/GameEngine/Commands/Command.cpp
+++ src/GameEngine/Commands/Command.cpp
@@ -1,6 +1,6 @@
 #include "Command.hpp"
 
-const std::string Command::latestBot = "JP36";
+const std::string Command::latestBot = "JP36_beatjp37";
 
 //to help with debugging...
 std::ostream & operator << (std::ostream &out, std::shared_ptr<Command> &cmd)
